42fb5bdd5b2fd4231a1f8cf6c77f244d88cbef7c
renoagil87 <renoagilsaputra87@gmail.com>
2020-06-20 14:43:51 +0700

    fix

--- page/tambah.php
+++ page/tambah.php
@@ -1,6 +1,7 @@
 <?php 
     require '../function/function.php';
-
+    
+    
 ?>
 
 <!DOCTYPE html>
@@ -19,40 +20,40 @@
                 <th>Nama</th>
                 <td>:</td>
                 <td>
-                    <input type="text" name="nama" id="">
+                    <input type="text" name="nama" id="" required>
                 </td>
             </tr>
             <tr>
                 <th>NIM</th>
                 <td>:</td>
                 <td>
-                    <input type="text" name="nim" id="">
+                    <input type="text" name="nim" id="" required>
                 </td>
             </tr>
             <tr>
                 <th>Jurusan</th>
                 <td>:</td>
                 <td>
-                    <input type="text" name="jurusan" id="">
+                    <input type="text" name="jurusan" id="" required>
                 </td>
             </tr>
             <tr>
                 <th>Alamat</th>
                 <td>:</td>
                 <td>
-                    <input type="text" name="alamat" id="">
+                    <input type="text" name="alamat" id="" required>
                 </td>
             </tr>
             <tr>
                 <th>Gambar</th>
                 <td>:</td>
                 <td>
-                    <input type="file" name="gambar" id="">
+                    <input type="file" name="gambar" id="" required>
                 </td>
             </tr>
             <tr>
                 <td colspan="3">
-                    <input type="submit" value="Tambah">
+                    <input type="submit" value="Tambah" name="tambah">
                 </td>
             </tr>
         </table>
